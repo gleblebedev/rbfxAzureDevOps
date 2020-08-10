1927932a05460e1f163c7320e5caeff6d71eedd6
Gleb Lebedev <gleb@gleblebedev.com>
2020-08-10 23:32:59 +0100

    Extracting branch name from env variable

--- build/Scripts/GenVersion.ps1
+++ build/Scripts/GenVersion.ps1
@@ -9,7 +9,17 @@ Write-Host "Last version template file commit: $lastCommit"
 [int]$gitTreeHeight = & git rev-list --first-parent --count "$lastCommit..HEAD"
 Write-Host "Git tree height since last version update: $gitTreeHeight"
 
-$gitBranchName = & git rev-parse --abbrev-ref=strict HEAD
+if ($Env:BUILD_SOURCEBRANCH) {
+    $gitBranchName = $Env:BUILD_SOURCEBRANCH
+    $prefix = 'refs/heads/'
+    if ($gitBranchName.StartsWith($prefix)) {
+        $gitBranchName = $gitBranchName.Substring($prefix.Length);
+    }
+}
+else {
+    $gitBranchName = & git rev-parse --abbrev-ref=strict HEAD
+}
+
 $gitCurrentCommit = & git rev-parse --short HEAD
 Write-Host "Current branch $gitBranchName at commit $gitCurrentCommit"
 
