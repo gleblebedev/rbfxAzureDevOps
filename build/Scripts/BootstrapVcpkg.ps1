071318a31abeac37cdfaeaaeb1d91ae3e7d640f5
Gleb Lebedev <gleb@gleblebedev.com>
2020-08-20 23:57:24 +0100

    vcpkg update

--- build/Scripts/BootstrapVcpkg.ps1
+++ build/Scripts/BootstrapVcpkg.ps1
@@ -2,7 +2,10 @@ $vcpkgFolder = Get-Item $PSScriptRoot/../../rbfx/vcpkg
 
 If ($IsWindows) {
     Invoke-Expression "$vcpkgFolder/bootstrap-vcpkg.bat"
+    Invoke-Expression "$vcpkgFolder/vcpkg.exe update"
 }
 else {
     Invoke-Expression "$vcpkgFolder/bootstrap-vcpkg.sh"
+    Invoke-Expression "$vcpkgFolder/vcpkg update"
 }
+
