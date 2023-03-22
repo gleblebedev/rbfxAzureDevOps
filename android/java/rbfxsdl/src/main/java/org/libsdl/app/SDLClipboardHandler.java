cdb5d579576989758fa4adb09270201de585015e
Gleb <gleb@gleblebedev.com>
2023-03-22 10:28:06 +0000

    SDL android update

--- android/java/rbfxsdl/src/main/java/org/libsdl/app/SDLClipboardHandler.java
+++
@@ -1,9 +0,0 @@
-package org.libsdl.app;
-
-public interface SDLClipboardHandler {
-
-    public boolean clipboardHasText();
-    public String clipboardGetText();
-    public void clipboardSetText(String string);
-
-}
